240de9aac33d22043cb5a7fba0d88b017344be64
fedeloz <fedeelozanomalaga@gmail.com>
2023-11-20 09:37:05 +0100

    Picasso update

--- SimulationRL.py
+++ SimulationRL.py
@@ -73,19 +73,19 @@ if len(physical_devices) > 0:
 
 # HOT PARAMS
 pathings    = ['hop', 'dataRate', 'dataRateOG', 'slant_range', 'Q-Learning', 'Deep Q-Learning']
-pathing     = pathings[1]# dataRateOG is the original datarate. If we want to maximize the datarate we have to use dataRate, which is the inverse of the datarate
+pathing     = pathings[5]# dataRateOG is the original datarate. If we want to maximize the datarate we have to use dataRate, which is the inverse of the datarate
 
-drawDeliver = True      # create pictures of the path every 1/10 times a data block gets its destination
+drawDeliver = False      # create pictures of the path every 1/10 times a data block gets its destination
 Train       = True      # Global for all scenarios with different number of GTs. if set to false, the model will not train any of them
 MIN_EPSILON = 0.01       # Minimum value that the exploration parameter can have 
-importQVals = False     # imports either QTables or NN from a certain path
+importQVals = True     # imports either QTables or NN from a certain path
 explore     = True      # If True, makes random actions eventually, if false only exploitation
-mixLocs     = False      # If true, every time we make a new simulation the locations are going to change their order of selection
+mixLocs     = True      # If true, every time we make a new simulation the locations are going to change their order of selection
 
 # number of gateways to be tested
-GTs = [2]
+# GTs = [10]
 # GTs = [i for i in range(2,19)] # 19.
-# GTs = [i for i in range(2,10)] # 19.
+GTs = [i for i in range(2,11)] # 19.
 
 CurrentGTnumber        = -1    # This number will be updating as the number of Gateways change. In the simulation it will iterate the GTs list
 
