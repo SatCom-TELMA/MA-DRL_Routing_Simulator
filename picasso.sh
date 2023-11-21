9a22cac7dda21a44724f0bf3d3587c89358b43f8
fedeloz <fedeelozanomalaga@gmail.com>
2023-11-21 09:04:03 +0100

    Picasso update. Test 2 secs bad cases

--- picasso.sh
+++ picasso.sh
@@ -2,13 +2,13 @@
 # -*- coding: utf-8 -*-
 
 # The name to show in queue lists for this job:
-#SBATCH -J DQL_{1024}_{2-10}
+#SBATCH -J DQL_{256}_{2}
 
 # Number of desired cpus:
 #SBATCH --cpus-per-task=1
 
 # Amount of RAM needed for this job:
-#SBATCH --mem=1024gb
+#SBATCH --mem=256gb
 
 # The time the job will be running:
 #SBATCH --time=100:00:00
