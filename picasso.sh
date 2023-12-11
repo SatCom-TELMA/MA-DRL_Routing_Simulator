8fc1f234b1ec78cac6c0538198b5d1aafbdc6c74
Federico Lozano <fedeelozanomalaga@gmail.com>
2023-12-11 18:47:56 +0100

    Test 4 Malaga inuvik no arrive reward more gamma

--- picasso.sh
+++ picasso.sh
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # The name to show in queue lists for this job:
-#SBATCH -J DQL_{128}_{2}_5_IN
+#SBATCH -J DQL_{128}_{2}_6_IN
 
 # Number of desired cpus:
 #SBATCH --cpus-per-task=1
