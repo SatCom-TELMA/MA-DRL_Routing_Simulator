c7c19a0cba0c3dd78266d8d2ca4b440062d507c5
Federico Lozano <fedeelozanomalaga@gmail.com>
2024-01-08 20:51:46 +0100

    Lab update

--- picasso.sh
+++ picasso.sh
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # The name to show in queue lists for this job:
-#SBATCH -J DQL_{128}_{2}_8
+#SBATCH -J DQL_{128}_{2}_9
 
 # Number of desired cpus:
 #SBATCH --cpus-per-task=1
