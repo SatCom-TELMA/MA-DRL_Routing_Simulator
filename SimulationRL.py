6af6825672de803064c3b5a361062888fe1cba60
fedeloz <fedeelozanomalaga@gmail.com>
2024-01-15 22:13:33 +0100

    PIcasso update

--- SimulationRL.py
+++ SimulationRL.py
@@ -97,8 +97,8 @@ diff        = False     # If up, the state space gives no coordinates about the 
 coordGran   = 1         # Granularity of the coordinates that will be the input of the DNN: (Lat/coordGran, Lon/coordGran)
 reducedState= True     # if set to true the DNN will receive as input only the positional information, but not the queueing information
 
-w1          = 3         # rewards the getting to empty queues
-w2          = 20        # rewards getting closes phisically    
+w1          = 5         # rewards the getting to empty queues
+w2          = 20        # rewards getting closes phisycally    
 ArriveReward= 50        # Reward given to the system in case it sends the data block to the satellite linked to the destination gateway
 
 latBias     = 0        # This value is added to the latitude of each position in the state space. This can be done to avoid negative numbers
