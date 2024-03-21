2794a401589e9734a4274a8c15918ccfd40f09a0
Federico Lozano <fedeelozanomalaga@gmail.com>
2024-03-21 12:02:06 +0100

    Congested scenario for picasso

--- picasso.sh
+++ picasso.sh
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # The name to show in queue lists for this job:
-#SBATCH -J DQL_{1024}_{8}
+#SBATCH -J DQL_{1024}_{8}_congestion
 
 # Number of desired cpus:
 #SBATCH --cpus-per-task=1
