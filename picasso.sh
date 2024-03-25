7b97d17c7c6304e93c8ac28fecb53012fc01a440
fedeloz <fedeelozanomalaga@gmail.com>
2024-03-25 02:39:27 +0100

    Mac. 8GTs congestion

--- picasso.sh
+++ picasso.sh
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # The name to show in queue lists for this job:
-#SBATCH -J DQL_{1024}_{8}_NoCong
+#SBATCH -J DQL_{1024}_{8}_CongNewV2
 
 # Number of desired cpus:
 #SBATCH --cpus-per-task=1
