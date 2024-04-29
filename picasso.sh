fca4266a2d5fe1625dec48fbfca6c70cb2a02dc9
fedeloz <fedeelozanomalaga@gmail.com>
2024-04-29 18:59:30 +0200

    6s 0.1 traffic offline train 8 GTs

--- picasso.sh
+++ picasso.sh
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # The name to show in queue lists for this job:
-#SBATCH -J DQL_{1024}_{8}_1s_online
+#SBATCH -J DQL_{1024}_{8}_6s_01
 
 # Number of desired cpus:
 #SBATCH --cpus-per-task=1
